a195ff5f161e408555484a541b90fca7858a61a6
TheRockMilad <therock.milad2@outlook.com>
2024-08-09 00:40:23 +0330

    Added another way to route with validation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Fastify from "fastify";
 const fastify = Fastify({
   logger: {
-    level : "trace"
+    level : ""
   }
 });
 
@@ -9,6 +9,47 @@ fastify.get("/", (req, reply) => {
   return {
     message: "hello world",
   };
+});
+
+//امکانات بیشتر میده
+fastify.route({
+  method: "GET", // نوع  درخواست
+  url: "/hello/:name", // مسیر روت ، نام یک پارامتر است
+  // اسکیما یا قالب برای اعتبارسنجی ورودی‌ها و خروجی‌های این روت
+  schema: {
+    // قالب پارامترهای ورودی (در اینجا فقط یک پارامتر بنام نام است
+    params: {
+      type: "object", // تعیین می‌کند که پارامترها باید یک آبجکت باشند.
+      //ویژگی‌های آبجکت پارامتره
+      properties: {
+        //  پارامتری از نوع (رشته) است
+        name: {
+          type: "string",
+          //   pattern: "^[A-Za-z]+$", // فقط حروف الفبایی
+        },
+      },
+      required: ["name"], //: مشخص می‌کند که پارامتر نام الزامی است.
+    },
+    // قالب خروجی برای پاسخ
+    response: {
+      //  قالب برای زمانی که پاسخ اچ تی ام ال با کد وضعیت 200 ارسال می‌شود
+      200: {
+        type: "object", //  پاسخ باید یک آبجکت باشد.
+        //  ویژگی‌های این آبجکت پاسخ.
+        properties: {
+          message: { type: "string" }, //ین ویژگی باید از نوع رشته باشد.
+        },
+        required: ["message"], // درخواست : ["پیام"]: مشخص می‌کند که ویژگی پیام باید حتماً در پاسخ وجود داشته باشد
+      },
+    },
+  },
+  handler: (req, reply) => {
+    //   const name = (req.params as { name: string }).name;
+    return {
+      //@ts-ignore
+      message: `Hello ${req.params.name}`,
+    };
+  },
 });
 
 try {
