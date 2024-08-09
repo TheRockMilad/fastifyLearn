b92116012fc2f11d83773d7cff9d35ae5de2cdf0
TheRockMilad <therock.milad2@outlook.com>
2024-08-09 17:43:39 +0330

    second get route

--- src/greetings-controller.ts
+++ src/greetings-controller.ts
@@ -1,11 +1,52 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import fastify, {
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+  FastifySchema,
+} from "fastify";
 
-const controller = (fastify: FastifyInstance, options: any, done: Function) => {
-  fastify.get("/", options, (req: FastifyRequest, reply: FastifyReply) => {
-    return {
-      message: "hello world",
-    };
-  });
+interface Params {
+  name: string;
+}
+
+const responseSchema: FastifySchema = {
+  response: {
+    200: {
+      type: "object",
+      properties: {
+        message: { type: "string" },
+      },
+    },
+  },
+};
+
+//  controller
+
+const controller = (
+  fastify: FastifyInstance,
+  options: any,
+  done: () => void
+) => {
+  fastify.get(
+    "/",
+    options,
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      return {
+        message: "hello world",
+      };
+    }
+  );
+
+  fastify.get(
+    "/:name",
+    { schema: responseSchema },
+    (req: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+      const { name } = req.params;
+      return {
+        message: `hello ${name}`,
+      };
+    }
+  );
   done();
 };
 
